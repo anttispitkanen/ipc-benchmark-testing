61a48ea42c748a12a009e0c8690ccd3a3a79cec4
Antti Pitkänen <antti.pitkanen@futurice.com>
2021-04-22 18:19:59 +0300

    Script for running whole test suite

---
+++ run-test-suite.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+
+NPM_TEST_COMMANDS=('test:benchmark' 'test:http')
+
+MOCK_DATA_SIZES=('small' 'medium' 'large')
+
+# Run each test permutation 5 times
+
+for command in "${NPM_TEST_COMMANDS[@]}"; do
+  for size in "${MOCK_DATA_SIZES[@]}"; do
+    for i in {1..5}; do
+      MOCK_DATA_SIZE=$size npm run $command
+    done
+  done
+done
